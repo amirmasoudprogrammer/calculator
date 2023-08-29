c564eaf6d208652cea5fee5f02b0537dea128128
amirmasoud asaditalab <mramirmasoudprogrammer.com>
2023-08-29 10:04:44 +0330

    programming clac

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 let display=document.querySelector(".display");
 let showdisplay=document.querySelectorAll(".show-display");
+let calc_ulate=document.querySelector(".calculate");
+let all_clear=document.querySelector(".all-clear");
+all_clear.addEventListener("click",startallclear);
+function startallclear() {
+    display.innerText= 0
+}
+calc_ulate.addEventListener("click",startcalculate);
+function startcalculate() {
+    let result=display.innerText;
+    display.innerText=eval(result)
+}
 showdisplay.forEach(function (item){
     item.addEventListener("click",startshow);
 })
